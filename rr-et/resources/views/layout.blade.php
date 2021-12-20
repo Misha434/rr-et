1e845b127b99785e34f4f0cfea709b811531a890
misha434 <missymouse434@gmail.com>
2021-12-20 15:09:42 +0900

    Add header in layout.blade.php (#2)
    ヘッダーを layout.blade.php に追加

--- rr-et/resources/views/layout.blade.php
+++ rr-et/resources/views/layout.blade.php
@@ -8,6 +8,24 @@
   <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 </head>
 <body>
+  <header>
+    <!-- Navbar start -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">RRET</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="#">Features</a>
+          <a class="nav-item nav-link" href="#">Pricing</a>
+          <a class="nav-item nav-link disabled" href="#">Disabled</a>
+        </div>
+      </div>
+    </nav>
+    <!-- Navbar end -->
+  </header>
   <main>
     @yield('content')
   </main>
