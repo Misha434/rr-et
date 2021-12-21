4f97df44d21aec905a9ddbad64339337146ca804
misha434 <missymouse434@gmail.com>
2021-12-21 09:44:26 +0900

    Add button scripts#destroy (#6)
    投稿削除ボタンを作成

--- rr-et/resources/views/scripts/index.blade.php
+++ rr-et/resources/views/scripts/index.blade.php
@@ -14,8 +14,22 @@
     <div class="row">
       @foreach($scripts as $script) 
         <div class="col-12">
-          <div class="card mt-2 p-2">
+          <div class="card mt-2 px-3 pt-3">
             <p>{{ $script->content }}</p>
+            <span class="border"></span>
+            <div class="float-right">
+              <!-- 削除ボタン Start -->
+              <form action="{{ route('scripts.destroy', $script->id) }}"
+              method="post" class="float-right mt-1 mb-3"
+              >
+              @csrf
+              @method('delete')
+              <input type="submit" value="削除" class="btn btn-danger btn-sm" 
+              onclick='return confirm("削除しますか？");'
+              >
+            </form>
+            <!-- 削除ボタン End -->
+            </div>
           </div>
         </div>
       @endforeach
