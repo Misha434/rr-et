132c971aaf0318d138ab7085cc5e146f4a1de2ad
misha434 <missymouse434@gmail.com>
2021-12-22 19:04:17 +0900

    Add posted username in scripts#index page (#22)
    投稿したユーザーをネタ一覧ページに表示

--- rr-et/resources/views/scripts/index.blade.php
+++ rr-et/resources/views/scripts/index.blade.php
@@ -34,30 +34,38 @@
           <div class="card mt-2 px-3 pt-3">
             <p>{{ $script->content }}</p>
             <span class="border"></span>
-            <div class="float-right">
-              <!-- 削除ボタン Start -->
-              <form action="{{ route('scripts.destroy', $script->id) }}"
-                    method="post" class="float-right mt-1 mb-3"
+            <div class="d-block">
+              <div class="float-left">
+                <div class="d-flex">
+                  <p class="mt-2">{{ $script->user->name }}</p>
+                  <p class="mt-2 mx-2 d-none d-sm-block" style="color:gray;">{{ $script->created_at }}</p>
+                </div>
+              </div>
+              <div class="float-right">
+                <!-- 削除ボタン Start -->
+                <form action="{{ route('scripts.destroy', $script->id) }}"
+                method="post" class="float-right mt-1 mb-3"
+                >
+                @csrf
+                @method('delete')
+                <input type="submit" value="削除" 
+                       class="btn btn-danger btn-sm" 
+                       onclick='return confirm("削除しますか？");'
+                >
+              </form>
+              <!-- 削除ボタン End -->
+              
+              <!-- 編集ボタン Start -->
+              <a href="{{ route('scripts.edit', $script->id) }}"
+                 class="btn btn-info btn-sm text-white float-right mt-1 mb-3 mx-2"
               >
-              @csrf
-              @method('delete')
-              <input type="submit" value="削除" 
-                     class="btn btn-danger btn-sm" 
-                     onclick='return confirm("削除しますか？");'
-              >
-            </form>
-            <!-- 削除ボタン End -->
-
-            <!-- 編集ボタン Start -->
-            <a href="{{ route('scripts.edit', $script->id) }}"
-               class="btn btn-info btn-sm text-white float-right mt-1 mb-3 mx-2"
-            >
               編集
             </a>
             <!-- 編集ボタン End -->
-            </div>
+          </div>
           </div>
         </div>
+      </div>
       @endforeach
     </div>
     <!-- Script End -->
