879636754901110f8902ec2ccee766b44830828b
misha434 <missymouse434@gmail.com>
2021-12-25 14:44:28 +0900

    Add action categories#store (#10)
    カテゴリー追加 保存のて目のアクションを追加

--- rr-et/app/Http/Controllers/CategoryController.php
+++ rr-et/app/Http/Controllers/CategoryController.php
@@ -35,9 +35,14 @@ class CategoryController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CreateCategory $request)
     {
-        //
+        $category = new Category();
+
+        $category->name = $request->name;
+        $category->save();
+
+        return redirect()->route('categories.index');
     }
 
     /**
