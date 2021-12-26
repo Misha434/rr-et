e329ca23375ffbc395985b5d78c6bd20453a5fac
misha434 <missymouse434@gmail.com>
2021-12-26 18:29:43 +0900

    Add 403 error page (#29)

---
+++ rr-et/resources/views/errors/403.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>403 Forbidden</title>
+</head>
+<body>
+  <div>
+    <h1>403 Forbidden</h1>
+    <p>You do not have access.</p>
+  </div>
+</body>
+</html>
