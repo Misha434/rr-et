bbaf64d93f3519f447aeeb7d1d00fcae6259f7ad
misha434 <missymouse434@gmail.com>
2022-01-02 16:15:18 +0900

    Modify header (#46)
    ヘッダーを修正

--- rr-et/resources/views/layout.blade.php
+++ rr-et/resources/views/layout.blade.php
@@ -10,71 +10,79 @@
 </head>
 <body>
   <header>
-    <!-- Navbar start -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="{{ route('home') }}">RRET</a>
-      <button class="navbar-toggler" type="button"
-              data-toggle="collapse" data-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent" aria-expanded="false"
-              aria-label="{{ __('Toggle navigation') }}"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <!-- Left Side Of Navbar -->
-        <ul class="navbar-nav mr-auto">
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="{{ route('scripts.index') }}">scripts</a>
-
-            </li>
-          </div>
-        </ul>
-
-        <!-- Right Side Of Navbar -->
-        <ul class="navbar-nav ml-auto">
-            <!-- Authentication Links -->
-            @guest
+  <div class="container-fluid g-0 bg-dark navbar-dark navbar-expand-lg">
+      <div class="row g-0">
+        <div class="col-3 col-lg-8 order-lg-1">
+          <button class="navbar-toggler" type="button"
+            data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="{{ __('Toggle navigation') }}"
+          >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-nav">
+          <div id="navbarSupportedContent" class="collapse navbar-collapse">
+            <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                <a class="nav-link {{ request()->route()->named('home*') ? ' active' : '' }}" href="{{ route('home') }}">ホーム</a>
               </li>
-              @if (Route::has('register'))
-                <li class="nav-item">
-                  <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                </li>
-              @endif
-            @else
-              <li class="nav-item dropdown">
-                <a id="navbarDropdown" class="nav-link dropdown-toggle"
-                   href="#" role="button" data-toggle="dropdown"
-                   aria-haspopup="true" aria-expanded="false" v-pre
-                >
-                  {{ Auth::user()->name }} <span class="caret"></span>
-                </a>
-
-                <div class="dropdown-menu dropdown-menu-right"
-                     aria-labelledby="navbarDropdown">
-                  <a class="dropdown-item" href="{{ route('logout') }}"
-                     onclick="event.preventDefault();
-                              document.getElementById('logout-form').submit();"
-                  >
-                    {{ __('Logout') }}
-                  </a>
-
-                  <form id="logout-form" action="{{ route('logout') }}"
-                        method="POST" style="display: none;"
-                  >
-                    @csrf
-                  </form>
-                </div>
+              <li class="nav-item">
+                <a class="nav-link {{ request()->route()->named('categories*') ? ' active' : '' }}" href="{{ route('categories.index') }}">カテゴリー</a>
               </li>
-            @endguest
-        </ul>
+              <li class="nav-item">
+                <a class="nav-link {{ request()->route()->named('scripts*') ? ' active' : '' }}" href="{{ route('scripts.index') }}">ネタ一覧</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        </div>
+        <div class="col-6 col-lg-2 order-lg-0">
+          <nav class="navbar-dark">
+            <div class="container d-flex align-items-center">
+              <a href="#" class="navbar-brand">
+                RR-ET
+              </a>
+            </div>
+          </nav>
+        </div>
+        <div class="col-3 col-lg-2 order-lg-2">
+          @guest
+            <div class="dropdown mr-3">
+              <a class="btn btn-dark dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Not login yet
+              </a>
+
+              <div class="dropdown-menu dropdown-menu-right"
+                  aria-labelledby="dropdownMenuLink">
+                <a class="dropdown-item" href="{{ route('login') }}">{{ __('ログイン') }}</a>
+                @if (Route::has('register'))
+                  <a class="dropdown-item" href="{{ route('register') }}">{{ __('新規登録') }}</a>
+                @endif
+              </div>
+            </div>
+          @else
+            <div class="dropdown">
+              <a class="btn btn-dark dropdown-toggle" href="#" role="button"
+                id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true"
+                aria-expanded="false"
+              >{{ Auth::user()->name }}</a>
+
+              <div class="dropdown-menu dropdown-menu-right"
+                  aria-labelledby="dropdownMenuLink">
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                  onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();"
+                >{{ __('ログアウト') }}</a>
+
+                <form id="logout-form" action="{{ route('logout') }}"
+                      method="POST" style="display: none;"
+                >@csrf</form>
+              </div>
+            </div>
+          @endguest
+        </div>
       </div>
-    </nav>
-    <!-- Navbar end -->
+    </div>
   </header>
   <main>
     @yield('content')
