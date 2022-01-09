6836d22d9f9f92678f6a97502886a7802ab577a9
misha434 <missymouse434@gmail.com>
2022-01-09 15:01:07 +0900

    Modify routing for like feat (#60)
    いいね機能のためにルーティングを設定

--- rr-et/routes/web.php
+++ rr-et/routes/web.php
@@ -3,6 +3,8 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use Whoops\Run;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -24,11 +26,16 @@ Route::get('/login/guest', 'Auth\LoginController@guestLogin');
 
 Route::group(['middleware' => ['auth', 'can:general-user']], function () {
   Route::resource('scripts', 'ScriptController');
-  Route::post('ajaxlike', 'ScriptController@ajaxlike')->name('scripts.ajaxlike');
   Route::resource('categories', 'CategoryController');
   Route::get('/users/{id}', 'UserController@show')->name('users.show');
   Route::get('/users/{id}/edit', 'UserController@edit')->name('users.edit');
   Route::put('/users/{id}', 'UserController@update')->name('users.update');
+  Route::group(['prefix'=>'scripts/{id}'],function(){
+    Route::get('/like/store','likeController@store');
+    Route::post('/like/store','likeController@store')->name('likes.store');
+    Route::get('/like/store','likeController@destroy');
+    Route::delete('/like/destroy','likeController@destroy')->name('likes.destroy');
+  });
 });
 
 Route::group(['middleware' => ['auth', 'can:admin']], function () {
