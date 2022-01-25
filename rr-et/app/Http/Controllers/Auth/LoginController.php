d10d5827c70c696607d8a1b6c91764ca7db32dbb
misha434 <missymouse434@gmail.com>
2022-01-26 00:21:40 +0900

    Execute codesniffer, Modify codes
    コード自動修正と一行が長いコードを改行するため

--- rr-et/app/Http/Controllers/Auth/LoginController.php
+++ rr-et/app/Http/Controllers/Auth/LoginController.php
@@ -54,7 +54,8 @@ class LoginController extends Controller
 
         if ($guestUser = User::where('email', $email)->first()) {
             Auth::loginUsingID($guestUser->id, true);
-            return redirect()->route('scripts.index')->with('status', 'ゲストユーザーとしてログインしました。');;
+            return redirect()->route('scripts.index')->with('status', 'ゲストユーザーとしてログインしました。');
+            ;
         } else {
             $createdGuestUser = User::create([
                 'name' => "Guest",
